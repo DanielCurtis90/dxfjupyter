bda7148f42f93092fa5fb764d93adbb71fe0f36f
Daniel Curtis <danielcurtis0@gmail.com>
2017-11-09 22:02:10 +0000

    added mysql logger for visiting IPs

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'python1root!'
+app.config['MYSQL_DATABASE_DB'] = 'ipdata'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+	requestip = request.remote_addr
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO ipdata (ip) VALUES ('" + requestip + "')")
+	conn.commit()
+	return 'Index Page'
 
 @app.route('/hello')
 def hello():
-    """Returns the html template index.html in 
-    the templates folder"""
-    return render_template('hello.html')
+	"""Returns the html template index.html in 
+	the templates folder"""
+	return render_template('hello.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=4999, debug=True)
