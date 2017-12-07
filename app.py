5a27a66c9bce0e1b23dd73f352bb871f8db71806
Daniel Curtis <danielcurtis0@gmail.com>
2017-12-07 22:19:49 +0000

    Added file uploader

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect
+from werkzeug.utils import secure_filename
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app = Flask(__name__)
+app.config.from_object('config')
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'python1root!'
 app.config['MYSQL_DATABASE_DB'] = 'ipdata'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+from helpers import *
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
-
 	return "Index Page"
 
 @app.route('/hello')
@@ -27,5 +36,32 @@ def hello():
 
 	return render_template('hello.html', ip_list=ip_list)
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+	if request.method=='POST':
+		print("Attempting Post")
+		# Case A, check that the file input key is there
+		if "user_file" not in request.files:
+			return "No user_file key in request.files"
+
+		# Case B, if the key is there, save it
+		file = request.files["user_file"]
+
+		# Case C, make sure there is a file
+		if file.filename == "":
+			return "Please select a file"
+
+		# Case D, check if authorized
+		if file and allowed_file(file.filename):
+			file.filename = secure_filename(file.filename)
+			output = upload_file_to_s3(file, app.config["S3_BUCKET"])
+			return str("{}{}".format(app.config["S3_LOCATION"], file.filename))
+
+		else:
+			return redirect("/upload")
+	else:
+		print(app.config["S3_LOCATION"])
+		return render_template('upload.html')
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=4999, debug=True)
+	app.run(host='0.0.0.0',debug=True, port=4999)
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+S3_BUCKET = os.environ.get("S3_DXF_BUCKET_NAME")
+S3_KEY = os.environ.get("S3_ACCESS_KEY")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+
+SECRET_KEY = os.urandom(32)
+DEBUG = True
+PORT = 4999
+HOST = '0.0.0.0'
